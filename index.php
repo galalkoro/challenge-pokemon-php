b3b56976c5f495a22b7afd43cbe5dabe9fc82125
galalkoro <koro-galal@outlook.com>
2022-06-14 16:37:00 +0200

    php index modified fetch api

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-<?php
-$dataBase = "https://pokeapi.co/api/v2/pokemon/";
 
-for ($id = 1; $id < 6; $id++) { 
-   $data = file_get_contents($dataBase.$id.'/');
-   $pokemon = json_decode($data);
-   echo $pokemon->name,"<br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-}
-?>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>The pokemon challenge - PHP style</title>
+</head>
+<body>
+    <main class="container">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Pokemon Logo">
+    
+        <form action="" class="search">
+          <input type="search" class="search-input" placeholder="Search about a pokemon using name or pokedex number ID">
+          <button class="search-button">Search</button>
+        </form>
+    
+        <section class="pokemon-container">
+        </section>
+        <span class="error">Not Found!</span>
+      </main>
+
+
+    <script src="./scripts/index.js"></script>
+</body>
+</html>
